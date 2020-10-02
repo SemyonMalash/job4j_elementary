08cb814d8cb2b4a95994fdefa3e80e16170c47d7
SemyonMalash <9217861121@mail.ru>
2020-10-02 05:04:48 +0300

    5.7. Ипотека [#365418]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+        while (amount > 0) {
+            double rsl = percent * amount;
+            amount += rsl - salary;
+            year++;
+        }
+        return year;
+    }
+}
